881715ee02b123b59e920b812de8943c7d50f191
engebeni <39270529+engebeni@users.noreply.github.com>
2018-05-19 21:17:10 +0200

    included basic wait

--- shell.c
+++ shell.c
@@ -12,13 +12,18 @@ char input[512] = { 0 };
 void printPrompt();
 void readInput();
 void parseInput(char*, char**);
-void executeCommand(char**);
-int waiting(pid_t ID);
+void executeCommand(char**, int);
+//void waiting(char**);
+int waiting(pid_t);
+void signalHandlerRegular(int);
 
 int main(void)
 {
+
 	char line[1024];
+	
 	char entryDir[512] = { 0 };
+	int background = 0;
 
 	if (getcwd(entryDir, sizeof(entryDir)) == NULL)
 	{
@@ -26,83 +31,73 @@ int main(void)
 		//EXIT?		
 	}
 
-	//while (fgets(line, sizeof(line), stdin) != NULL)
 	while(1)
-	{
+	{	
+		signal(SIGINT, signalHandlerRegular);
 		printPrompt();
 
 		if (fgets(line, sizeof(line), stdin) == NULL)
 		{
 			perror("Error while reading from stdin:");
 			//EXIT?
-		}
+		}		
 
-		char *args[64];
-        	char **next = args;
-        	char *temp = strtok(line, " \n");
-        	while (temp != NULL)
-        	{
-        		*next++ = temp;
-			temp = strtok(NULL, " \n");
-        	}
-
-        	*next = NULL;
-
-		if (strcmp(args[0], "exit") == 0)
-			exit(EXIT_SUCCESS);
-		else if (strcmp(args[0], "cd") == 0)
+		char *pnt = line;
+		if (*pnt != '\n')
 		{
-			if (args[1] == NULL)
+			char *args[64];
+        		char **next = args;
+        		char *temp = strtok(line, " \n");
+        		while (temp != NULL)
+        		{
+				if (strcmp(temp, "&") == 0)
+				{
+					background = 1;
+					*next++ = NULL;
+				}
+        			else
+				{			
+					*next++ = temp;
+				}
+				temp = strtok(NULL, " \n");
+        		}
+	
+        		*next = NULL;
+
+			if (strcmp(args[0], "exit") == 0)
+				exit(EXIT_SUCCESS);
+			else if (strcmp(args[0], "cd") == 0)
 			{
-				if (chdir(entryDir) < 0)
-					perror("Error while changing directory:");
-			}		
+				if (args[1] == NULL)
+				{
+					if (chdir(entryDir) < 0)
+						perror("Error while changing directory:");
+				}		
+				else
+				{
+					if (chdir(args[1]) < 0)
+						perror("Error while changing directory:");
+				}
+			}
+			else if (strcmp(args[0], "wait") == 0)
+			{
+				int j = 1;
+				while (args[j] != NULL)
+				{
+					printf("%d\n", waiting(atoi(args[j++]))); 
+				}
+			}
 			else
 			{
-				if (chdir(args[1]) < 0)
-					perror("Error while changing directory:");
+        			executeCommand(args, background);
 			}
-		}
-		else if (strcmp(args[0], "wait") == 0)
-		{	
-			for (int i=1, i!=NULL, i++)
-			{
-				waiting(atoi(args[i]));
-			}
-		}
-		else
-		{
-        		executeCommand(args);
-		}
-    	}
+    		}	
 
+	}
+	background = 0;
+		
     return EXIT_SUCCESS;	
 }
-/***********************************************************************************************/
-
-int waiting(int ID) 
-{ 
-  int status; 
-  int j = waitpid(ID, &status,WNOHANG);
- 
-    if (j<0) 									//waitpid returned error
-    {   
-    	perror("ERROR");
-    } 
-    if (j==0)									//status not available
-    {
-    	perror("status not available \n");
-    }
-	else 										//status available
-	{
-		printf("waitpid(): %d\n", j);
-		printf("status: %d\n", status);
-	}   
- 
-
- // printf("%d \n", status);
-  return status && WIFEXITED(status) && WIFSIGNALED(stat_val)&& WTERMSIG(stat_val) ; 	
-} 
 
 void printPrompt()
 {
@@ -126,24 +121,14 @@ void printPrompt()
 	printf("%s", host);
 }
 
-void executeCommand(char **inArguments)
+void executeCommand(char **inArguments, int inBackground)
 {
-
-	int i = 0;
-	int background = 0;
-
-	while(inArguments[i] != NULL)
-	{	
-		if (strcmp(inArguments[i++], "&") == 0)
-			   background = 1;
-	}
 
 	pid_t child_pid = fork(); /* fork a new process */
 	if (child_pid < 0)
 		fprintf(stderr, "Fork Failed");
 	else if (child_pid == 0)
 	{
-		 printf("PID: %i \n",getpid()); 
 		signal(SIGINT, SIG_IGN);
 		if (execvp(*inArguments, inArguments) < 0)
 		{ /* execute cmd */
@@ -153,9 +138,9 @@ void executeCommand(char **inArguments)
 	}
 	else
 	{
-		if (background == 1)
+		if (inBackground == 1)
 		{
-			printf("PID[%d]\n", child_pid);	
+			printf("PID[%d]\n", child_pid);
 		}
 		else
 		{
@@ -167,4 +152,48 @@ void executeCommand(char **inArguments)
 		}
 	}
 
+}
+
+int waiting(pid_t ID) 
+{ 
+ 	int status; 
+	int j = waitpid(ID, &status,WNOHANG);
+
+	if (j == -1)
+		printf
+		while (wait(&status) != ID)
+		{
+			//do nothing
+		}
+/*
+	
+
+	while (waitpid(ID, &status,WNOHANG) != ID)
+	{
+		printf("waiting...");
+	}
+
+
+	if (j < 0) 									//waitpid returned error
+	{   
+	   	perror("Error while waiting:");
+	} 
+	if (j == 0)									//status not available
+	{
+		perror("Status not available:");
+	}
+	else 										//status available
+	{
+		printf("waitpid() = %d\n", j);
+		printf("status: %d\n", status);
+	}
+*/
+ 
+	return status && WIFEXITED(status) && WIFSIGNALED(status) && WTERMSIG(status) ; 	
+} 
+
+//ignore CTRL+C when not waiting
+void signalHandlerRegular(int sig)
+{
+	signal(sig, SIG_IGN);
 }
